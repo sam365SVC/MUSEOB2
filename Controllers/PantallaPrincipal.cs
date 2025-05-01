a8dab8110d82c03fb2e366fc51d0efad7afa7dc8
sam365SVC <svillcacastro@gmail.com>
2025-04-30 23:39:02 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/PantallaPrincipal.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MUSEOB2.Controllers
+{
+    public class PantallaPrincipal : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
